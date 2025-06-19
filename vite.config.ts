c1008b44f4c602cc912f852ef142883e27271ace
nir-hl <79724978+nirli-439@users.noreply.github.com>
2025-06-19 19:29:30 +0300

    feat: add 3d lanyard component

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
+  assetsInclude: ['**/*.glb'],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
