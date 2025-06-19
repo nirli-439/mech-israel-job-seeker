62905bf5453655fa049970284f679775a518383a
nir-hl <79724978+nirli-439@users.noreply.github.com>
2025-06-19 19:35:32 +0300

    Revert "Add 3D Lanyard component"

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -6,13 +6,3 @@ declare namespace JSX {
   }
 }
 
-declare module '*.glb' {
-  const src: string;
-  export default src;
-}
-
-declare module '*.png' {
-  const src: string;
-  export default src;
-}
-
