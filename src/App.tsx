427a090c388af6ebba4c8ce77f087f6278bb30cf
nir-hl <nir2010@gmail.com>
2025-07-15 10:51:11 +0300

    clean

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
 
 import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import BallpitBackground from "@/components/reactbits/BallpitBackground";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 
@@ -12,22 +9,17 @@ const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <div className="relative min-h-screen">
-        <BallpitBackground />
-        <div className="relative z-10">
-          <Toaster />
-          <Sonner />
-          <BrowserRouter>
-            <Routes>
-              <Route path="/" element={<Index />} />
-              {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </BrowserRouter>
-        </div>
+    <div className="relative min-h-screen">
+      <div className="relative z-10">
+        <Toaster />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
       </div>
-    </TooltipProvider>
+    </div>
   </QueryClientProvider>
 );
 
